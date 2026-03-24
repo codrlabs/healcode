16aac63ebb98812e0ad802da100be19444bd21fd
Samuel Olabode <solabode499@gmail.com>
2026-03-24 03:48:11 -0600

    chore: update package.json description and fix script formatting; add health check endpoint in index.js according to the PR comments #20

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const path = require('path');
 const PORT = process.env.PORT || 3000;
+
+app.get('/health', (req, res)=>{
+    res.status(200).json({status: 'okay', message : "Server is running!"});
+})
 
 app.listen(PORT, () => {  
     console.log(`Server is running on port ${PORT}`);
