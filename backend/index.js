bfa49d15804a16f64a66deee30a6f97bc8c28239
Samuel Olabode <solabode499@gmail.com>
2026-03-26 21:54:22 -0600

    chore: update health check endpoint to include error handling and fix script paths in package.json #20 #22

--- backend/index.js
+++ backend/index.js
@@ -4,8 +4,13 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/health', (req, res)=>{
-    res.status(200).json({status: 'okay', message : "Server is running!"});
-})
+    try{
+        res.status(200).json({status: 'okay', message: "Server is running!"});
+    }catch(err){
+        console.error(err);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+});
 
 app.listen(PORT, () => {  
     console.log(`Server is running on port ${PORT}`);
